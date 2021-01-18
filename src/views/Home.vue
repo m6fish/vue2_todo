6686962d8277fa7b95bd6b5dae285591ac332ff6
bulb <pumpkinjack@livemail.tw>
2021-01-18 22:59:15 +0800

    頁面粗底

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="home">
+      <div>
+          <input type="text"> <button>Add</button>
+      </div>
+      <ul>
+          <li>
+                <div>
+                    <input type="checkbox">
+                    <label>456</label>
+                    <button>delete</button>
+                </div>
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
     name: 'Home',
-    components: {
+    data () {
+        return {
+            list: [{
+                id: 1,
+                text: 'my todo',
+                status: 1
+            }]
+        }
+    },
+    computed: {
+        // 取得過濾後的代辦清單
+        getFilterList () {
+            return []
+        }
+    },
+    methods: {
+        // 新增代辦
+        addTodo () {
+
+        },
+        // 刪除指定代辦
+        deleteTodo () {
+
+        },
+        // 更新指定代辦
+        updateTodo () {
+
+        }
     }
 }
 </script>
